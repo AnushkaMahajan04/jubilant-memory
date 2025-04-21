e6f3cfdfc966643b3426f928a70ca14be26dac82
AnushkaMahajan04 <anumahajan1804@gmail.com>
2025-04-21 22:32:38 +0530

    Add files via upload

---
+++ Testcalci.java
@@ -0,0 +1,12 @@
+package debug;
+
+public class Testcalci {
+	public static void main(String[] args) {
+		Calculatordebug c2= new Calculatordebug();
+		c2.addition(20, 30);
+		c2.subtract(30, 20);
+		c2.multiply(5, 7);
+		c2.Division(8, 4);
+	}
+
+}
