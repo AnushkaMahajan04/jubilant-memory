9b0611ee3aacab9f76360cbde1529b97c19a0e8c
AnushkaMahajan04 <anumahajan1804@gmail.com>
2025-04-21 20:45:56 +0530

    Add files via upload

---
+++ Calculator1.java
@@ -0,0 +1,27 @@
+package project2;
+
+public class Calculator1 {
+	int price = 999;
+	String cname = "CASIO";
+
+	void addition(int n1, int n2) {
+		int sum = n1 + n2;
+		System.out.println("Addition=" + sum);
+	}
+
+	void subtract(int n1, int n2) {
+		int sub = n1 - n2;
+		System.out.println("Subraction =" + sub);
+	}
+
+	void multiply(int n1, int n2) {
+		int mul = n1 * n2;
+		System.out.println("Multiplication=" + mul);
+	}
+
+	void Division(int n1, int n2) {
+		int div = n1 / n2;
+		System.out.println("Division=" + div);
+	}
+
+}
---
+++ Test2.java
@@ -0,0 +1,12 @@
+package project2;
+
+public class Test2 {
+	public static void main(String[] args) {
+		Calculator1 c1 = new Calculator1();
+		c1.addition(20, 30);
+		c1.subtract(30, 20);
+		c1.multiply(5, 7);
+		c1.Division(8, 4);
+	}
+
+}
