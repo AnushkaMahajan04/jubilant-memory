85597491bcbab2da563c4e52eb94724d7bd7417f
AnushkaMahajan04 <anumahajan1804@gmail.com>
2025-04-22 18:52:14 +0530

    Add files via upload

---
+++ PuneRTO.java
@@ -0,0 +1,11 @@
+
+public class PuneRTO {
+	void CheckAge(int a) {
+		if(a>18) {
+			System.out.println("you are eligible");
+			}else {
+				System.out.println("wait for="+(18-a)+"years");
+			}
+	}
+
+}
---
+++ TestRTO.java
@@ -0,0 +1,9 @@
+
+public class TestRTO {
+	public static void main(String [] args) {
+		int jay=10;
+		PuneRTO u1=new PuneRTO();
+		u1.CheckAge(jay);
+	}
+
+}
