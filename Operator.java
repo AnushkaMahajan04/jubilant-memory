79ef2b7519b85196a6e736f9b0dfa9475fd02c18
AnushkaMahajan04 <anumahajan1804@gmail.com>
2025-05-05 22:25:44 +0530

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,68 @@
+package com.tka.opeartors1;
+
+public class Operator {
+	public static void main(String [] args) {
+		int a=10,b=5;
+		System.out.println("Arithmetic for integer:");
+		System.out.println("a+b="+(a+b));
+		System.out.println("a-b="+(a-b));
+		System.out.println("a*b="+(a*b));
+		System.out.println("a/b="+(a/b));
+		System.out.println("a%b="+(a%b));
+		
+		float f1 = 5.5f, f2 = 2.2f;
+		System.out.println("Arithmetic for float:");
+		System.out.println("f1+f2="+(f1+f2));
+		System.out.println("f1-f2="+(f1-f2));
+		System.out.println("f1*f2="+(f1*f2));
+		System.out.println("f1/f2="+(f1/f2));
+		System.out.println("f1%f2="+(f1%f2));
+		
+		double x = 10.5, y = 4.2;
+		System.out.println("Arithmetic for double:");
+		System.out.println("x+y="+(x+y));
+		System.out.println("x-y="+(x-y));
+		System.out.println("x*y="+(x*y));
+		System.out.println("x/y="+(x/y));
+		System.out.println("x%y="+(x%y));
+		
+		System.out.println("Comparison Operators with integer:");
+        System.out.println("a > b: " + (a > b));
+        System.out.println("a < b: " + (a < b));
+        System.out.println("a == b: " + (a == b));
+        System.out.println("a != b: " + (a != b));
+        
+        System.out.println("Comparison Operators with float:");
+        System.out.println("f1 >f2: " + (f1 > f2));
+        System.out.println("f1 < f2: " + (f1 < f2));
+        System.out.println("f1== f2: " + (f1 == f2));
+        System.out.println("f1 != f2: " + (f1 != f2));
+
+        System.out.println("Comparison Operators with double:");
+        System.out.println("x > y: " + (x > y));
+        System.out.println("x < y: " + (x < y));
+        System.out.println("x == y: " + (x == y));
+        System.out.println("x!= y: " + (x != y));
+        
+        boolean p = true, q = false;
+        System.out.println("Logical Operators:");
+        System.out.println("p && q: " + (p && q));
+        System.out.println("p || q: " + (p || q));
+        System.out.println("!p: " + (!p));
+
+
+
+
+
+
+		
+
+
+
+
+		
+		
+	}
+	
+
+}
